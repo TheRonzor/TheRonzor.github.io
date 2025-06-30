b99d73a80433e337b3988a751321ea994ce7499e
Ron <ronniecougar@gmail.com>
2025-06-30 15:35:25 -0400

    archived old site

--- zz_sandbox/rz_fourier.js
+++
@@ -1,7 +0,0 @@
-// Auxilliary functions for the Visual Fourier page
-function get_center_of_mass(xvals, yvals) {
-    const arrAvg = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
-    xmean = 2*arrAvg(xvals);
-    ymean = 2*arrAvg(yvals);
-    return [xmean, ymean]
-}
